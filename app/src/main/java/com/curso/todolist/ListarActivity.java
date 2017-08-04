2532ec99ec18ba491dbfcf3157a0a300e552ddb9
danieloliveira138 <danieloliveira138@gmail.com>
2017-08-04 03:45:49 -0300

    Mudança no ListView. Falta melhorar o Layout do ListView no Arquivo xml Erro no Primeiro Push

--- app/src/main/java/com/curso/todolist/ListarActivity.java
+++ app/src/main/java/com/curso/todolist/ListarActivity.java
@@ -3,7 +3,6 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
